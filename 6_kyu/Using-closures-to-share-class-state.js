01013d8edaf7ecf725b0f22206cbf8ccca678a6c
Radionov_Vitaly <radionovv89@gmail.com>
2022-12-04 16:37:47 +0300

    Using closures to share class state

---
+++ 6_kyu/Using-closures-to-share-class-state.js
@@ -0,0 +1,16 @@
+let Cat = (function () {
+  let catCount = 0, catAggWeight = 0;
+  let constr = function (name, weight) {
+    if (!name || !weight) {
+      throw 'Must provide a name and a weight!';
+    }
+    catCount++; catAggWeight += weight;
+    Object.defineProperty(this, 'weight', {
+      set: function (v) {
+        catAggWeight += v - weight; weight = v;
+      }, get: function () { return weight; }
+    });
+  }
+  constr.averageWeight = function () { return catAggWeight / catCount; }
+  return constr;
+}());
